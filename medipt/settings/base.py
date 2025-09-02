ef3929cf9e2609c40a49a776cd6933322be5b381
Ajani Timothy <timothyinioluwa@gmail.com>
2025-09-02 12:55:03 +0100

    fix: added project setups for prodcution and development

--- medipt/settings/base.py
+++ medipt/settings/base.py
@@ -92,15 +92,7 @@ CACHES = {
 }
 
 
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
--- medipt/settings/development.py
+++ medipt/settings/development.py
@@ -7,14 +7,23 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_ALL_ORIGINS = True
 
 
-# Email Configuration (base - override in environment files)
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = env("DEVELOPMENT_EMAIL_HOST", default="smtp.gmail.com")
-EMAIL_PORT = env("DVELOPMENT_EMAIL_PORT", default=587, cast=int)
-EMAIL_HOST_USER = env("DEVELOPMENT_EMAIL_HOST_USER", default="")
-EMAIL_HOST_PASSWORD = env("DEVELOPMENT_EMAIL_HOST_PASSWORD", default="")
-EMAIL_USE_TLS = True
+EMAIL_HOST = env("DEV_EMAIL_HOST")
+EMAIL_PORT = env("DEV_EMAIL_PORT")
+EMAIL_HOST_USER = env("DEV_EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("DEV_EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = env("DEFAULT_FROM_EMAIL")
+EMAIL_USE_TLS = True
 
 
 FRONTEND = ""
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('DEV_DB_NAME'),
+        'USER': env('DEV_DB_USER'),
+        'PASSWORD': env('DEV_DB_PASSWORD'),
+        'HOST': env('DEV_DB_HOST'),
+        'PORT': env('DEV_DB_PORT'),
+    }
+}
